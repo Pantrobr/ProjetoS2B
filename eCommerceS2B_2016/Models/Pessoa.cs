a581afea49937e3be7dbed9d461a66bec03ffacc
Pantrobr <pantrobr@gmail.com>
2016-09-28 14:25:30 -0300

    pessoa

---
+++ eCommerceS2B_2016/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eCommerceS2B_2016.Models
+{
+    public abstract class Pessoa
+    {
+        #region "Atributos"
+
+        #endregion
+
+        #region "Construtores"
+        /// <summary>
+        /// Classe Pessoa é abstrata e define os elementos básicos que um registro de usuario ou administrador deve ter
+        /// </summary>
+        /// <param name="nome">Primeiro nome do cadastrado</param>
+        /// <param name="sobrenome">Sobrenome do cadastrado</param>
+        /// <param name="cpf">CPF - provavelmente terá alguma regra especifica de validação</param>
+        /// <param name="login">o login deve ser um string do formato email@email.com</param>
+        /// <param name="senha">string com o password</param>
+        public Pessoa(string nome, string sobrenome, string cpf, string login, string senha)
+        {
+
+        }
+        #endregion
+    }
+}
