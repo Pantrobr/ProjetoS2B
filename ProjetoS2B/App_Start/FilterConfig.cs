f89153081f6ed630e9122684d28337492086aeb0
Rafael <Rafael@RAFA-NOTE2>
2016-10-13 15:23:37 -0300

    Add project files.

---
+++ ProjetoS2B/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoS2B
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
